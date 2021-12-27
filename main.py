68ba05c91f8cee1a5a0eb4fd0c69fe0019b446c1
Omar Shawky <ipowerfuliii@gmail.com>
2021-12-27 23:30:09 +0200

    added flag -I

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ class FileSearchExtension(Extension):
 
         """ Searches for Files using fd command """
         cmd = [
-            'timeout', '5s', 'ionice', '-c', '3', bin_name, '--threads', '1',
-#            '--hidden'
+            'timeout', '5s', 'ionice', '-c', '3', bin_name, '--threads', '1','-I'
+            #            '--hidden'
         ]
 
         if file_type == FILE_SEARCH_FILE:
